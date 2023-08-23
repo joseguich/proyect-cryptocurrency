993784352e119b9c32b4cd5dc64ac047ccb626dc
joseguich <joseguichardomieses@gmail.com>
2023-08-23 09:29:37 -0400

    Selection of HTML elements.

---
+++ js/const.js
@@ -0,0 +1,9 @@
+export const $ = (selector) => document.querySelector(selector);
+export const $createElement = (create) => document.createElement(create);
+
+export const $cryptocurrency = $("#cryptocurrency");
+export const $currency = $("#currency");
+export const $form = $("#form");
+export const $exit = $("#exit");
+export const $showWebsite = $("#list-table");
+export const $disableButton = $('button[type="submit"]');
