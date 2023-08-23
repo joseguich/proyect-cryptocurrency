c80f7a84eba3d242ed41c1995165b70fe04aee33
joseguich <joseguichardomieses@gmail.com>
2023-08-23 09:35:15 -0400

    Validate the fields that are not empty.

---
+++ js/func/validator-select.js
@@ -0,0 +1,21 @@
+import { fillObjectCrypto } from "./fill-object.js";
+import { showAlert } from "../func/show-alert.js";
+import { getConsultCrypto } from "../API/fetch.js";
+import { buttonDisable } from "./button-disable.js";
+
+export const validatorSelect = (e) => {
+  e.preventDefault();
+
+  const { currency, cryptocurrency } = fillObjectCrypto;
+
+  if (currency === "" || cryptocurrency === "") {
+    showAlert("Fields are required select the currency and cryptocurrency");
+    return;
+  }
+
+  //Get API.
+  getConsultCrypto();
+
+  //Disable Button.
+  buttonDisable();
+};
